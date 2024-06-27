16fd498459ff19ee11b946269c02add8d4b986b5
Steven Samuel <samuelsteven6@gmail.com>
2024-06-27 17:19:12 +0800

    Change buttons to be pressed instead of button up

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,14 +1,12 @@
 extends Node2D
 
-func _on_play_game_button_up():
-	SoundEffects.click_audio_play()
-	get_tree().change_scene("res://Levels/Level Selection/level_selection_scene.tscn")
-
-
 func _on_settings_game_button_up():
 	SoundEffects.click_audio_play()
 
+func _on_play_game_pressed():
+	SoundEffects.click_audio_play()
+	get_tree().change_scene("res://Levels/Level Selection/level_selection_scene.tscn")
 
-func _on_quit_game_button_up():
+func _on_quit_game_pressed():
 	SoundEffects.click_audio_play()
 	get_tree().quit()
