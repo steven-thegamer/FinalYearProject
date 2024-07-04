21673deedfb810488ae55e28919f23e2bc68734d
Steven Samuel <samuelsteven6@gmail.com>
2024-07-05 05:37:47 +0800

    polishing again

--- display_level.gd
+++ display_level.gd
@@ -2,13 +2,18 @@ extends Node2D
 
 onready var desc = $DescriptionText
 onready var tit = $TitleText
+onready var anim = $AnimationPlayer
 
 var scene_path : String
 var level_code_selection : String
 
+const typewriter_speed_per_second := 30.0
+
 func update_text(title_text : String,description_text :  String):
 	tit.text = title_text
 	desc.bbcode_text = description_text
+	desc.percent_visible = 0
+	anim.play("show_animation")
 
 func _on_Button_pressed():
 	if scene_path != "":
@@ -21,3 +26,8 @@ func _on_Button_pressed():
 func _on_close_button_pressed():
 	scene_path = ""
 	hide()
+
+func typewriter_description() -> void:
+	while desc.percent_visible < 1:
+		desc.visible_characters += 1
+		yield(get_tree().create_timer(1.0/typewriter_speed_per_second),"timeout")
